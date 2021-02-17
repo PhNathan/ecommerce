cc6ca4577d249e2dffc6bdee20cb9ea2e78c30df
PhNathan <phelipe-nathandf@hotmail.com>
2021-02-17 10:54:51 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -22,6 +24,8 @@ $app->get('/', function() {
 
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
@@ -29,6 +33,36 @@ $app->get('/admin', function() {
 
 	
 
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["deslogin"], $_POST["despassword"]);
+	/*Paramos aqui no dia 12/02/2021*/
+
+	header("Location: /admin");
+	exit;
+
+});
+
+
+$app->get('/admin/logout', function(){
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
--- views-cache/login.d70a64ba7abb00fe427240570e3a4447.rtpl.php
+++ views-cache/login.d70a64ba7abb00fe427240570e3a4447.rtpl.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
@@ -27,32 +27,32 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="/res/admin/index2.html"><b>Admin</b>LTE</a>
+    <a href="/res/admin/index2.html"><b>Admin </b>Dev</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
+    <p class="login-box-msg">Logue para iniciar sua sessão</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="/admin/login" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Login" name="deslogin">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Password" name="despassword">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox icheck">
             <label>
-              <input type="checkbox"> Remember Me
+              <input type="checkbox"> Lembrar-me
             </label>
           </div>
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Logar</button>
         </div>
         <!-- /.col -->
       </div>
@@ -60,15 +60,15 @@
 
     <div class="social-auth-links text-center">
       <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Logar com
         Facebook</a>
-      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
+      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Logar com
         Google+</a>
     </div>
     <!-- /.social-auth-links -->
 
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
+    <a href="#">Esqueci minha senha :(</a><br>
+    <a href="register.html" class="text-center">Não tem conta ainda, cadastre-se !!!</a>
 
   </div>
   <!-- /.login-box-body -->
@@ -76,11 +76,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery 2.2.3 -->
-<script href="/res/admin/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="/res/admin/plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
-<script href="/res/admin/bootstrap/js/bootstrap.min.js"></script>
+<script src="/res/admin/bootstrap/js/bootstrap.min.js"></script>
 <!-- iCheck -->
-<script href="/res/admin/plugins/iCheck/icheck.min.js"></script>
+<script src="/res/admin/plugins/iCheck/icheck.min.js"></script>
 <script>
   $(function () {
     $('input').iCheck({
