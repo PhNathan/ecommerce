2f37b210265bc061428a9564bbbb7a590c69af9b
PhNathan <phelipe-nathandf@hotmail.com>
2021-03-26 16:43:36 -0300

    alterar senha

--- site.php
+++ site.php
@@ -622,9 +622,91 @@ $app->get("/profile/orders/:idorders", function($idorder){
 		'products'=>$cart->getProducts()
 	]);
 
+});
 
+
+$app->get("/profile/change-password", function(){
+
+		User::verifyLogin(false);
+
+		$page = new Page();
+
+
+		$page->setTpl("profile-change-password", [
+			'changePassError'=>User::getError(),
+			'changePassSuccess'=>User::getSuccess()
+		]);
 
 
 });
+
+
+
+$app->post("/profile/change-password", function(){
+
+	User::verifyLogin(false);
+
+	if (!isset($_POST['current_pass']) || $_POST['current_pass'] === '') {
+
+		User::setError("Digite sua senha atual !!");
+
+		header("Location: /profile/change-password");
+
+		exit;
+	}
+
+	if (!isset($_POST['new_pass']) || $_POST['new_pass'] === '') {
+
+		User::setError("Digite sua nova senha  !!");
+
+		header("Location: /profile/change-password");
+
+		exit;
+	}
+
+	if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === '') {
+
+		User::setError("Confirme a nova senha !!");
+
+		header("Location: /profile/change-password");
+
+		exit;
+	}
+
+
+	if ($_POST['current_pass'] === $_POST['new_pass']) {
+
+		User::setError("Digite uma senha diferente da antiga !!");
+
+		header("Location: /profile/change-password");
+
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	if (!password_verify($_POST['current_pass'], $user->getdespassword())) {
+		
+		User::setError("Senha Inválida !!");
+
+		header("Location: /profile/change-password");
+
+		exit;
+
+
+	}
+
+	$user->setdespassword($_POST['new_pass']);
+
+	$user->update();
+
+	User::setSuccess("Senha Alterada com Sucesso");
+
+	header("Location: /profile/change-password");
+	exit;
+
+});
+
+
 
 ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -175,7 +175,7 @@ public function update( $changePassword = true )
     $sql = new Sql();
     $results = $sql->select( "CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", 
                                   array( 
-                                  ":iduser"      => $this->getiduser(),
+                                 ":iduser"      => $this->getiduser(),
                                   ":desperson"   => utf8_decode( $this->getdesperson() ),
                                  ":deslogin"    => $this->getdeslogin(),
                                  ":despassword" => $password,
